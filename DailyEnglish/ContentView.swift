4e9f876cefe510f74adf82ae9cdd6680d2cd3117
Masato Ishii <sattsat141@icloud.com>
2023-10-11 17:21:52 +0900

    Initial Commit

---
+++ DailyEnglish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyEnglish
+//
+//  Created by 石井大翔 on 2023/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyEnglish/DailyEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyEnglishApp.swift
+//  DailyEnglish
+//
+//  Created by 石井大翔 on 2023/10/11.
+//
+
+import SwiftUI
+
+@main
+struct DailyEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
