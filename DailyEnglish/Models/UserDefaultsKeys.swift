01c0148d97bdb75c64f8176e71ef5104689a5d70
sattsat <syuuatit141offer@gmail.com>
2024-12-10 19:43:12 +0900

    UserDefaultsの保存を行うためのKeyを宣言

---
+++ DailyEnglish/Models/UserDefaultsKeys.swift
@@ -0,0 +1,11 @@
+//
+//  UserDefaultsKeys.swift
+//  DailyEnglish
+//  
+//  Created by SATTSAT on 2024/12/10
+//  
+//
+
+enum SettingKeys: String {
+    case questionFormat //出題形式　英→日（true）, 日→英(false)
+}
